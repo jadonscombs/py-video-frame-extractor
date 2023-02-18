16a2a1fed9123b20d5dcee6c26dfeba235d3d1b4
jadonscombs <jadonscombs@gmail.com>
2023-02-18 11:18:35 -0800

    quick change

--- frame_extractor_driver.py
+++ frame_extractor_driver.py
@@ -8,20 +8,21 @@ choice (e.g. "png", "jpeg", etc.)
 Please see <frame_capture_components.py> for more details.
 """
 
-import sys
+from sys import stderr
 try:
-    import frame_capture_components as extractor
+    import frame_capture_components
     
 except:
     print(
         "[random video frame extractor] error: "
         "core file 'frame_capture_components.py' could not be loaded.",
-        file=sys.stderr
+        file=stderr
     )
+    raise SystemExit
 
 
 # program entry point
 if __name__ == '__main__':
-    arg_parser = extractor.init_parser()
+    arg_parser = frame_capture_components.init_parser()
     args = arg_parser.parse_args()
-    extractor.capture_frames_driver(args)
+    frame_capture_components.capture_frames_driver(args)
